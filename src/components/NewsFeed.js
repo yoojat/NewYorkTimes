8ed4c047dd694928b1100c5f26c59bb505039366
yoojat <yooja.ty@gmail.com>
2018-09-07 16:26:51 +0900

    FlatList

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Text, View, FlatList } from "react-native";
+import { List, ListItem } from "react-native-elements";
+
 import { COMMON_STYLES } from "../styles/global";
 export default class NewsFeed extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataSource: props.news,
+      modalVisible: false
+    };
+  }
+
   render() {
     return (
       <View style={COMMON_STYLES.pageContainer}>
-        <FlatList />
-        {/* ListView Deprcated */}
+        <List>
+          <FlatList
+            data={this.props.news}
+            renderItem={({ item }) => (
+              <ListItem
+                key={item.key}
+                roundAvatar
+                avatar={{ uri: item.imageUrl }}
+                title={item.title}
+                subtitle={item.subtitle}
+              />
+            )}
+          />
+          {/* ListView Deprcated */}
+        </List>
       </View>
     );
   }
@@ -21,22 +45,24 @@ NewsFeed.propTypes = {
 NewsFeed.defaultProps = {
   news: [
     {
+      key: "a",
       title: "React Native",
-      imageUrl: "https://facebook.github.io/react/img/logo_og.png",
+      imageUrl: "https://avatars3.githubusercontent.com/u/14242795?s=460&v=4",
       description: "Build Native Mobile Apps using JavaScript and React",
       date: new Date(),
       author: "Facebook",
       location: "Menlo Park, California",
-      url: "https://facebook.github.io/react-native"
+      url: "https://github.com/miso360"
     },
     {
+      key: "b",
       title: "Packt Publishing",
-      imageUrl: "https://www.packtpub.com/sites/default/files/packt_logo.png",
+      imageUrl: "https://avatars0.githubusercontent.com/u/25732075?s=460&v=4",
       description: "Stay Relevant",
       date: new Date(),
       author: "Packt Publishing",
       location: "Birmingham, UK",
-      url: "https://www.packtpub.com/"
+      url: "https://github.com/0131mj"
     }
   ]
 };
